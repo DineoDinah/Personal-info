94c8268efa1b23e1d4f27bc14a9d2489cac292c7
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-11 15:18:37 +0200

    Add project files.

---
+++ Personal info/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personal_info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write(" Enter your name\n ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine(" When you were born " + name);
+            int year = int.Parse(Console.ReadLine());
+
+            int age = 2023 - year;
+
+            Console.WriteLine();
+            Console.WriteLine(" You are " + age + " years old ");
+            Console.ReadLine();
+
+
+        }
+    }
+}
